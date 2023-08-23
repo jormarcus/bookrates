5e9aaabed0b60fb172fc98c52625c75ebe996f0b
jormarcus <jormarcus92@gmail.com>
2023-08-23 16:43:20 -0400

    Setting up cassandra connection

---
+++ src/test/java/com/bookrates/bookratesdataloader/BookratesDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookrates.bookratesdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookratesDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
